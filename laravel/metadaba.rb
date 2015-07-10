79d062cd25748ae6452989cdd4c95f65582ba144
Rohan <me@rohandey.com>
2015-07-10 09:43:34 +0530

    Add laravel

---
+++ laravel/metadaba.rb
@@ -0,0 +1,7 @@
+name              "laravel"
+maintainer        "Juan Manuel Torres"
+maintainer_email  "kinojman@gmail.com"
+license           "MIT"
+description       "Set of recipes to help setup PHP applications ."
+version           "1.0.0"
+
