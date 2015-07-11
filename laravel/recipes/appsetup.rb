9f2099a293de5ebd4e481f4e7c0b5c0d749dffef
Rohan <me@rohandey.com>
2015-07-11 13:28:18 +0530

    Update appsetup.rb

--- laravel/recipes/appsetup.rb
+++ laravel/recipes/appsetup.rb
@@ -9,6 +9,14 @@ node[:deploy].each do |app_name, deploy|
     chmod -R 777 storage
     EOH
   end
-    
+  if deploy[:application] == "platform"
+   script "set_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current/public"
+    code <<-EOH
+    chmod -R 777 uploads
+    EOH
+  end    
   end
 end
